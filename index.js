d3f2f74b696bcb00d2fec9b5feb844a588f62206
Savindu Shehan <savindushehansamaraweera@gmail.com>
2024-01-13 22:57:56 +0530

    add the file

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+var S1 = document.getElementById("s1");
+var S2 = document.getElementById("s2");
+var S3 = document.getElementById("s3");
+
+/*
+document.addEventListener('DOMContentLoaded', function () {
+  S1.onclick = function () {
+    import('./Components/Button_1.css');
+  };
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  S2.onclick = function () {
+    import('./Components/Button_2.css');
+  };
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  S3.onclick = function () {
+    import('./Components/Button_3.css');
+  };
+});
+*/
+
+document.getElementById('s1').addEventListener('click', function() {
+  console.log("Savindu1");
+  document.getElementById('cssLink').href = 'Components/Button_1.css';
+});
+document.getElementById('s2').addEventListener('click', function() {
+  console.log("Savindu2");
+  document.getElementById('cssLink').href = 'Components/Button_2.css';
+});
+document.getElementById('s3').addEventListener('click', function() {
+  console.log("Savindu3");
+  document.getElementById('cssLink').href = 'Components/Button_3.css';
+});
+
